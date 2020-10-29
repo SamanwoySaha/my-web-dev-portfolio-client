a0eea49ba2919b81ea5fc198e3de98ecccc2079c
SamanwoySaha <samanwoy.kuet@gmail.com>
2020-10-29 17:11:16 +0600

    added resume page

--- src/components/Resume/Resume/Resume.js
+++ src/components/Resume/Resume/Resume.js
@@ -1,12 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Header from '../../Shared/Header/Header';
+import { Document, Page } from 'react-pdf';
+import ResumePdf from '../../../pdf/Resume.pdf';
+import { Container } from 'react-bootstrap';
+import { FaFilePdf } from 'react-icons/fa';
 
 const Resume = () => {
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+        setPageNumber(1);
+    }
+
+    function changePage(offset) {
+        setPageNumber(prevPageNumber => prevPageNumber + offset);
+    }
+
+    function previousPage() {
+        changePage(-1);
+    }
+
+    function nextPage() {
+        changePage(1);
+    }
+
     return (
         <div>
             <Header></Header>
-            <h1>this is resume</h1>
+            <Container className="mx-auto mb-5 d-flex flex-column align-items-center">
+                <Document
+                    file={ResumePdf}
+                    options={{ workerSrc: "/pdf.worker.js" }}
+                    onLoadSuccess={onDocumentLoadSuccess}
+                    className="resume"
+                >
+                    <Page pageNumber={pageNumber} />
+                </Document>
+                <div className="d-flex flex-column justify-content-center align-items-center">
+                    <p>
+                        Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+                    </p>
+                    <div>
+                        <button className="main-btn" type="button" disabled={pageNumber <= 1} onClick={previousPage}>Previous</button>
+                        <button
+                            type="button"
+                            disabled={pageNumber >= numPages}
+                            onClick={nextPage}
+                            className="main-btn"
+                        >
+                            Next
+                    </button>
+                    </div>
+                    <button className="main-btn mt-3">
+                        <a href='https://drive.google.com/file/d/1sDs2G2lW3txrw5QsLxhjJ0xSHE5-ImWL/view?usp=sharing'>
+                            <FaFilePdf className="icon mr-1" /> Download Resume
+                        </a>
+                    </button>
+                </div>
+            </Container>
             <Footer></Footer>
         </div>
     );
