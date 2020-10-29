4ac741580c53b5c34e28d0c9da1b24eb3a5f3317
SamanwoySaha <samanwoy.kuet@gmail.com>
2020-10-29 19:44:52 +0600

    added blogs page

--- src/components/Shared/DisplayCard/DisplayCard.js
+++ src/components/Shared/DisplayCard/DisplayCard.js
@@ -14,16 +14,16 @@ const DisplayCard = ({ data }) => {
                 <h5 className="card-title">{name}</h5>
                 <p className="card-text">{description}</p>
                 <h6>{technologies.map(technology => <span className="mr-1 my-1 badge badge-info">{technology}</span>)}</h6>
-                <div className="card-footer d-flex justify-content-between">
+                <div className="card-footer d-flex align-items-center justify-content-between">
                     <a href={liveLink} className="card-footer-content">
                         {
-                            path === '/projects' ? <FaFirefoxBrowser className="icon mt-n1" /> Live Project
-                            : <FaFirefoxBrowser className="icon mt-n1" /> View Article
+                            path === '/projects' ? <><FaFirefoxBrowser className="icon mt-n1" /> Live Project</>
+                            : <><FaFirefoxBrowser className="icon mt-n1" /> View Article</>
                         }
                     </a>
                     {data.githubClient && <a href={data.githubClient} className="card-footer-content ml-auto mr-1"><FaGithub className="icon ml-1 mt-n1" /> Client</a>}
                     {data.githubServer && <a href={data.githubServer} className="card-footer-content"><FaGithub className="icon ml-1 mt-n1" /> Server</a>}
-                    {data.date && <p className="card-footer-content">{data.date}</p>}
+                    {data.date && <p className="date">{data.date}</p>}
                 </div>
             </div>
         </div>
