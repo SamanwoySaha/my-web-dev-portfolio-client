c6c888cb01a84fb2e3e6a54bbb51a41f487756ce
SamanwoySaha <samanwoy.kuet@gmail.com>
2020-10-31 18:01:33 +0600

    added image in noMatch page

--- src/components/NoMatch/NoMatch.js
+++ src/components/NoMatch/NoMatch.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './NoMatch';
+import Header from '../Shared/Header/Header';
 
 const NoMatch = () => {
     return (
         <div>
-            <h1>404 not found</h1>
+            <Header></Header>
+            <img src="https://i.imgur.com/nufX7I3.png" alt="" className="img-fluid w-100" style={{ height: '100vh'}}/>
         </div>
     );
 };
